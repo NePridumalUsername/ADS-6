823e39b28dae860e8ad3b717829e59b17081ac00
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-04-17 16:36:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,37 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T* arr;
+    int first;
+    int last;
+
+ public:
+    TPQueue() : first(0), last(0) { arr = new T[size]; }
+    void push(T x) {
+        if (last - first >= size) {
+            return;
+        } else {
+            int i = last++;
+            while (i != first && x.prior > arr[(i - 1) % size].prior) {
+                arr[i % size] = arr[(i - 1) % size];
+                i--;
+            }
+            arr[i % size] = x;
+        }
+    }
+    T pop() {
+        return arr[(first++) % size];
+    }
+    int getSize() {
+        return (last - first);
+    }
+    T front() {
+        return arr[first % size];
+    }
+    T back() {
+        return arr[(last - 1) % size];
+    }
 };
 
 struct SYM {
